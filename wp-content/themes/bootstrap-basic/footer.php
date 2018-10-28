a19b01c11d42cfb8858ead1cce33f820edb4ae3b
Niels Zwart <nielszwart@hotmail.com>
2018-10-28 14:26:22 +0100

    Add privacy statement link in footer

--- wp-content/themes/bootstrap-basic/footer.php
+++ wp-content/themes/bootstrap-basic/footer.php
@@ -9,7 +9,12 @@
 		</div><!-- page container -->
 		<footer class="footer" role="contentinfo">
 			<div id="footer-row" class="row site-footer">
-				<p class="copyright">&copy; <?php echo date('Y') ;?> G. Zwart Oefentherapie Mensendieck</p>
+				<span class="copyright">
+					&copy; <?php echo date('Y') ;?> G. Zwart Oefentherapie Mensendieck
+				</span>
+				<span class="privacy">
+					| <a href="/privacyverklaring">Privacy Verklaring</a>
+				</span>
 			</div>
 		</footer>
 
